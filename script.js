1f95950d62e1801834fc5b3d48c31d08cfcdf19a
Lucas Gomes <lucasdgomes15@gmail.com>
2023-06-13 22:57:53 -0300

    added weekly tasks

--- script.js
+++ script.js
@@ -36,16 +36,40 @@ function newTask()
     input.value = ''
 }
 
-function showValues()
-{
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    let list = document.getElementById('to-do-list')
-    list.innerHTML = ''
-    for(let i = 0; i < values.length; i++)
-    {
-        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
+function newWeeklyTask() {
+    let input = document.getElementById('input-new-task');
+    input.style.border = '';
+  
+    // validation
+    if (!input.value) {
+      input.style.border = '1px solid red';
+      alert('Digite algo para inserir em sua lista');
+    } else if (validateIfExistsNewTask()) {
+      alert('Já existe uma task com essa descrição');
+    } else {
+      // increment to localStorage
+      let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+      values.push({
+        name: input.value,
+        isWeekly: true
+      });
+      localStorage.setItem(localStorageKey, JSON.stringify(values));
+      showValues();
     }
-}
+    input.value = '';
+  }
+  
+
+  function showValues() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let list = document.getElementById('to-do-list');
+    list.innerHTML = '';
+  
+    for (let i = 0; i < values.length; i++) {
+      let taskClass = values[i].isWeekly ? 'weekly-task' : '';
+      list.innerHTML += `<li class="${taskClass}">${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`;
+    }
+  }
 
 function removeItem(data)
 {
